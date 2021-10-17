1fa0d3fc0111c20ea556e819b648dc03c44ec03b
Rafael Lefever <rafael.lefever@discomco.pl>
2021-10-17 08:49:27 +0200

    v1.10.1

--- init-context.sh
+++ init-context.sh
@@ -500,9 +500,7 @@ prepareRepo() {
     ### Install the SpecFlow Templates
     cd $target_dir
     dotnet new --install SpecFlow.Templates.DotNet::3.9.8
-    dotnet nuget remove source 'M5x SDK Nugets'
-    dotnet nuget remove source 'Logatron Nugets'
-    dotnet nuget add source "$sdk_nugets_url" -n 'M5x SDK Nugets' -u "$user" -p "$password" --store-password-in-clear-text
+    dotnet nuget remove source 'App Nugets'
     dotnet nuget add source "$logatron_nugets_url" -n 'Logatron Nugets' -u "$user" -p "$password" --store-password-in-clear-text
     
     git branch develop
